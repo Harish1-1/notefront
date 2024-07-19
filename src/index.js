a760b1ea35a9606d3aabbee4a1a8bc2e11d01540
Harish1-1 <109864814+Harish1-1@users.noreply.github.com>
2024-07-19 17:25:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  const WebSocketClient = require('./WebSocketClient');
+  WebSocketClient.initSocket();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
